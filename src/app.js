7d222ba173b8e479a5c1d88449718d0eabac2685
Adolfo Castro <castro09@gmail.com>
2021-08-21 02:21:43 -0500

    Solved many bugs and documented the code

--- src/app.js
+++ src/app.js
@@ -1,34 +1,69 @@
+/** Script that creates a stopwatch with its buttons
+ * @author Adolfo Castro <github.com/adolfo9castro/>
+ */
+
+/** Get the app DOM
+* @constant
+*/
 const app = document.getElementById('app')
 
+/** Get config and title
+* @constant
+*/
 const { InitTemplate, title } = config
 
+/** Initialiced variables
+* @variation
+*/
 let hours = 1,
     minutes = 1,
     secons = 1,
     milisecons = 1,
     id;
+
+// Draw the init HTML tags in the dom
 app.innerHTML = `<h3 class="display-4 text-center">${title}</h3>`
 app.innerHTML += `
     <div class="row timeContainer">
-    <span class="col timer hours">${hours === "00" ? 0 : "00"}</span>:
-    <span class="col timer minutes">${minutes === "00" ? 0 : "00"}</span>:
-    <span class="col timer secons">${secons === "00" ? 0 : "00"}</span>:
-    <span class="col timer milisecons">${milisecons === "00" ? 0 : "00"}</span>
+    <span class="col timer hours">00</span>:
+    <span class="col timer minutes">00</span>:
+    <span class="col timer secons">00</span>:
+    <span class="col timer milisecons">00</span>
     </div>`
 app.innerHTML += InitTemplate
 
+/** Create a buttons functionality 
+ * @constant
+*/
 const start = document.getElementById("start")
 const pause = document.getElementById("pause")
 const reset = document.getElementById("reset")
 
+/** Start counting
+ *  @function 
+ *  @returns {undefined}
+ */
 const startFunction = () => {
-    id = setInterval(cronometro, 10)
+    id = setInterval(init, 10)
+    start.removeEventListener("click", startFunction)
 }
+
+/** Pause the count
+ *  @function 
+ *  @returns {undefined}
+ */
 const pauseFunction = () => {
     clearInterval(id)
+    start.addEventListener('click', startFunction)
 }
+
+/** Reset and finish the count
+ *  @function 
+ *  @returns {undefined}
+ */
 const resetFunction = () => {
     clearInterval(id)
+    start.addEventListener('click', startFunction)
     hours = 1
     minutes = 1
     secons = 1
@@ -38,23 +73,35 @@ const resetFunction = () => {
     document.querySelector('.minutes').innerHTML = '00'
     document.querySelector('.hours').innerHTML = '00'
 }
-const cronometro = () => {
-    document.querySelector('.milisecons').innerHTML = milisecons++
+
+/** Initialize the stopwhatch
+ *  @function 
+ *  @returns {undefined}
+ */
+const init = () => {
+    document.querySelector('.milisecons').innerHTML = milisecons < 10 ? `0${milisecons++}` : milisecons++
     if (milisecons > 99) {
         milisecons = 0
+        document.querySelector('.milisecons').innerHTML = milisecons < 10 ? `0${milisecons++}` : milisecons++
         document.querySelector('.secons').innerHTML = secons < 10 ? `0${secons++}` : secons++
-        if (secons >= 59) {
-            secons = 0
-            document.querySelector('.minutes').innerHTML = minutes < 10 ? `0${minutes++}` : minutes++
-            if (minutes >= 59) {
-                minutes = 0
-                document.querySelector('.hours').innerHTML = hours < 10 ? `0${hours++}` : hours++
-            }
-        }
     }
-
+    if (secons > 60) {
+        secons = 0
+        document.querySelector('.secons').innerHTML = secons < 10 ? `0${secons++}` : secons++
+        document.querySelector('.minutes').innerHTML = minutes < 10 ? `0${minutes++}` : minutes++
+    }
+    if (minutes > 60) {
+        minutes = 0
+        document.querySelector('.minutes').innerHTML = minutes < 10 ? `0${minutes++}` : minutes++
+        document.querySelector('.hours').innerHTML = hours < 10 ? `0${hours++}` : hours++
+    }
+    if (hours > 60) {
+        hours = 0
+        document.querySelector('.hours').innerHTML = hours < 10 ? `0${hours++}` : hours++
+    }
 }
 
+// Add the event click to buttons to start the stopwhatch
 start.addEventListener('click', startFunction)
 pause.addEventListener('click', pauseFunction)
 reset.addEventListener('click', resetFunction)
